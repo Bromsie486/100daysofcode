fd51f93273af8466c286cec8ec955b9e26b04cc2
Bálint Skonda <skondab@gmail.com>
2023-11-15 14:42:59 +0100

    you can't quess the same letter now

--- hangman_game/hangman.py
+++ hangman_game/hangman.py
@@ -1,9 +1,11 @@
 import random
+import sys
 from hangman_database import words
 from hangman_art import logo
 class Hangman():
     def __init__(self):
         self.player_lives = 6
+        self.guessed_words = []
 
 
     def greet_player(self):
@@ -13,7 +15,6 @@ class Hangman():
     def choose_random_word(self):
         possible_words = words 
         word = random.choice(possible_words)
-        self.correct_choices_to_win = len(word)
         return word
 
 
@@ -28,9 +29,14 @@ class Hangman():
     def guess_word(self, blank_lines, word):
         #ask for input
         guess = input("Guess a letter! ").lower()
-        while len(guess) != 1:
-            guess = input("You have to guess exactly 1 letter. Please try again: ")
+        while len(guess) != 1 or guess in self.guessed_words:
+            if len(guess) != 1:
+                guess = input("You have to guess exactly 1 letter. Please try again: ")
+            else:
+                print(f"You already guessed the letter {guess}, please try a different one.")
+                guess = input("Guess a letter! ").lower()
 
+        self.guessed_words.append(guess)
         guessed_right = False
 
         for i, letter in enumerate(word):
@@ -41,7 +47,9 @@ class Hangman():
         if guessed_right:
             print("You guessed a letter!")
             print("".join(blank_lines))
-            self.correct_choices_to_win -= 1
+            if "_" not in blank_lines:
+                print("You win!")
+                sys.exit()
         else:
             self.player_lives -= 1
             print(f"You have lost a life. You have {self.player_lives} lives remaining.")
@@ -51,12 +59,10 @@ class Hangman():
         self.greet_player()
         word = self.choose_random_word()
         blank_lines = self.generate_blank_lines(word)
-        while self.player_lives > 0 and self.correct_choices_to_win > 0:
+        while self.player_lives > 0:
             self.guess_word(blank_lines, word)
         if self.player_lives == 0:
             print(f"Game over\nThe word was {word}")
-        else:
-            print("You win!")
 
 
 if __name__ == "__main__":
