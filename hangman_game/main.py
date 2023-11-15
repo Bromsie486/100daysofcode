b61851fb732d1342aa93586d7cb0831d7a6f2bcd
Bálint Skonda <balint.skonda@idomsoft.hu>
2023-11-15 09:08:09 +0100

    choose_random_word created

--- hangman_game/main.py
+++ hangman_game/main.py
@@ -1,14 +1,23 @@
-
+import random
 class Hangman():
 
     def __init__(self):
         self.player_lives = 6
 
+
     def greet_player(self):
         print("Welcome to Hangman!")
 
+
+    def choose_random_word(self):
+        possible_words = ["mouse", "snake", "zebra"]
+        return random.choice(possible_words)
+
+
     def main(self):
         self.greet_player()
+        print(self.choose_random_word())
+
 
 if __name__ == "__main__":
     hangman = Hangman()
