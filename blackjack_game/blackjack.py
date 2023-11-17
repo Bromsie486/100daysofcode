2810d117e27ef4315cb3233047fdbba65bbd6081
Bálint Skonda <skondab@gmail.com>
2023-11-17 18:48:34 +0100

    new cards can be added to player's hand now

--- blackjack_game/blackjack.py
+++ blackjack_game/blackjack.py
@@ -3,8 +3,8 @@ import os
 
 class Blackjack_Game():
 
-    def main(self):
-        value_table = {
+    def __init__(self):
+        self.value_table = {
             "2": 2,
             "3": 3,
             "4": 4,
@@ -18,56 +18,45 @@ class Blackjack_Game():
             "Q": 10,
             "K": 10,
         }
-        deck = []
-        deck = self.load_deck(deck)
-        while input("Do you want to play a round of Blackjack? (y/n) ") != "n":
-            os.system("clear")
-            player_hand = self.generate_hand(deck)
-            print(f"Your hand: {player_hand[0]}, {player_hand[1]}")
-            computer_hand, computer_hand_value = self.generate_computer_hand(deck, value_table)
-            print(f"Computer hand: {computer_hand[0]}, ?")
-            if input("Do you want another card? (y/n) ").lower() == "y":
-                player_hand.append(self.generate_hand(deck, 1)[0])
-                output_string = ""
-                for i in range(len(player_hand)):
-                    output_string += player_hand[i]
-                    if i != len(player_hand)-1:
-                        output_string += ", "
-                print(f"Your new hand: {output_string}")
-                    
+        self.deck = []
+        self.player_hand_value = 0
+        self.computer_hand_value = 0
 
-    def load_deck(self, deck):
+
+    def main(self):
+        self.load_deck()
+        while input("Do you want to play a game of Blackjack?(y/n) ").lower() != "n":
+            player_hand = self.generate_player_hand()
+            print(f"This is your hand: {player_hand[0]}, {player_hand[1]}")
+            while input("Do you want another card? (y/n) ").lower() != "n":
+                player_hand.append(self.generate_player_hand(1)[0])
+                output_text = self.generate_output(player_hand)
+                print(f"This is your new hand: {output_text}")
+
+
+    def generate_player_hand(self, amount_to_generate = 2):
+        generated_cards = []
+        for i in range(amount_to_generate):
+            card = random.choice(self.deck)
+            self.deck.pop(self.deck.index(card))
+            generated_cards.append(card)
+        return generated_cards
+
+
+    def load_deck(self):
         type_of_cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
         for i in range(32):
             for card_type in type_of_cards:
-                deck.append(card_type)
-        return deck
+                self.deck.append(card_type)
 
 
-    def generate_hand(self, deck, amount_to_generate = 2):
-        generated_cards = []
-        for i in range(amount_to_generate):
-            card = random.choice(deck)
-            deck.pop(deck.index(card))
-            generated_cards.append(card)
-        return generated_cards
-    
-
-    def generate_computer_hand(self, deck, value_table):
-        generated_cards = []
-        hand_value = 0
-        while hand_value <= 16:
-            card = random.choice(deck)
-            deck.pop(deck.index(card))
-            generated_cards.append(card)
-            if card != "A":
-                hand_value += value_table[card]
-            else:
-                if hand_value + 11 > 21:
-                    hand_value += 11
-                else:
-                    hand_value += 1
-        return generated_cards, hand_value
+    def generate_output(self, player_hand):
+        output_text = ""
+        for i in range(len(player_hand)):
+            output_text += player_hand[i]
+            if i != len(player_hand)-1:
+                output_text += ", "
+        return output_text
 
 
 if __name__ == "__main__":
