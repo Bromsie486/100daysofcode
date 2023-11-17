b3ec785069c60a6eac3612d46c2980b92382694b
Bálint Skonda <skondab@gmail.com>
2023-11-17 17:45:29 +0100

    generate_computer_hand created

--- blackjack_game/blackjack.py
+++ blackjack_game/blackjack.py
@@ -1,15 +1,40 @@
 import random
+import os
 
 class Blackjack_Game():
 
     def main(self):
+        value_table = {
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "5": 5,
+            "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9,
+            "10": 10,
+            "J": 10,
+            "Q": 10,
+            "K": 10,
+        }
         deck = []
         deck = self.load_deck(deck)
         while input("Do you want to play a round of Blackjack? (y/n) ") != "n":
-            generated_cards = self.generate_hand(deck)
-            print(f"Your hand: {generated_cards[0]}, {generated_cards[1]}")
-            print(f"Cards remaining in the deck: {len(deck)}")
-
+            os.system("clear")
+            player_hand = self.generate_hand(deck)
+            print(f"Your hand: {player_hand[0]}, {player_hand[1]}")
+            computer_hand, computer_hand_value = self.generate_computer_hand(deck, value_table)
+            print(f"Computer hand: {computer_hand[0]}, ?")
+            if input("Do you want another card? (y/n) ").lower() == "y":
+                player_hand.append(self.generate_hand(deck, 1)[0])
+                output_string = ""
+                for i in range(len(player_hand)):
+                    output_string += player_hand[i]
+                    if i != len(player_hand)-1:
+                        output_string += ", "
+                print(f"Your new hand: {output_string}")
+                    
 
     def load_deck(self, deck):
         type_of_cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
@@ -26,6 +51,23 @@ class Blackjack_Game():
             deck.pop(deck.index(card))
             generated_cards.append(card)
         return generated_cards
+    
+
+    def generate_computer_hand(self, deck, value_table):
+        generated_cards = []
+        hand_value = 0
+        while hand_value <= 16:
+            card = random.choice(deck)
+            deck.pop(deck.index(card))
+            generated_cards.append(card)
+            if card != "A":
+                hand_value += value_table[card]
+            else:
+                if hand_value + 11 > 21:
+                    hand_value += 11
+                else:
+                    hand_value += 1
+        return generated_cards, hand_value
 
 
 if __name__ == "__main__":
