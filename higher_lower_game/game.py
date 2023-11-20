2edb8cc3b0703cf012fe3d4abe2cf85fa9c03826
Bálint Skonda <skondab@gmail.com>
2023-11-20 11:15:57 +0100

    skeleton created

---
+++ higher_lower_game/game.py
@@ -0,0 +1,12 @@
+import random
+import os
+
+class Higher_Lower_Game():
+
+    def main():
+        pass
+
+
+if __name__ == "__main__":
+    higher_lower_game = Higher_Lower_Game()
+    higher_lower_game.main()
