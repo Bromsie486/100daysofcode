e94bf20bd146ce5e29adfcb80fc742bb0d4d5873
Bálint Skonda <skondab@gmail.com>
2023-11-20 11:45:00 +0100

    database created, vs logo added, comparison loop created

--- higher_lower_game/game.py
+++ higher_lower_game/game.py
@@ -1,6 +1,10 @@
 import random
 import os
 
+from art import main_logo, vs_logo
+from database import comparisons
+
+
 class Higher_Lower_Game():
 
     def __init__(self) -> None:
@@ -8,7 +12,19 @@ class Higher_Lower_Game():
 
 
     def main(self):
-        pass
+        while True:
+            os.system("clear")
+            print(main_logo)
+
+            comparison_a = random.choice(comparisons)
+            comparisons.remove(comparison_a)
+            comparison_b = random.choice(comparisons)
+            comparisons.append(comparison_a)
+
+            print("Compare A: {} - {}".format(comparison_a["name"], comparison_a["description"]))
+            print(vs_logo)
+            print("Against B: {} - {}".format(comparison_b["name"], comparison_b["description"]))
+            break
 
 
 if __name__ == "__main__":
